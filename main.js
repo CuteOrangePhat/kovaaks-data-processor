e3d09efbd4ac8263839788b6bcd00a5b9825d615
Jeremy Taghizadeh <jtaghiza@gmail.com>
2021-05-29 19:01:37 -0500

    feat: add csv to json converter

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const chokidar = require('chokidar');
 const path = require('path')
+const csvToJson = require('./utils/csvToJson')
+const fs = require('fs')
 
 const isDev = true;
 
+/*
+TODO: Can't read from program files because of a glob issue :( need to fix
+    https://github.com/paulmillr/chokidar/issues/300
+*/
 const statsPath = isDev ?
     './src/mockData/*.csv'
-    :'C:/Program Files (x86)/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer/stats/*.csv';
+    : "C:/Program Files (x86)/Steam/steamapps/common/FPSAimTrainer/FPSAimTrainer/stats/**/*.csv";
+
 
 const chokidarOptions = {
     depth: 1
 }
 
-function createWindow () {
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -31,9 +38,25 @@ function createWindow () {
     );
 
     win.webContents.on('did-finish-load', () => {
-        chokidar.watch(statsPath, chokidarOptions).on('all', (event, path) => {
-            win.webContents.send('toFileService', path)
+        chokidar.watch(statsPath, chokidarOptions).on('add', (path) => {
+            try {
+                const data = fs.readFileSync(path, 'utf8')
+                const {birthtime} = fs.statSync(path)
+                const jsonResult = {
+                    date: birthtime,
+                    event: 'add',
+                    data: csvToJson(data)
+                };
+                win.webContents.send('toFileService', jsonResult)
+            } catch (err) {
+                console.error(err)
+            }
         });
+
+        chokidar.watch(statsPath, chokidarOptions).on('unlink', (path) => {
+                win.webContents.send('toFileService', {event: 'unlink'})
+        });
+
     })
 }
 
